4fb6dbb3bedadeaec718b9ff4452ef71fb99c403
yakov <mirenkovye@yandex.ru>
2020-03-04 22:10:16 +0300

    Initial commit task1

---
+++ task1/solve.c
@@ -0,0 +1,41 @@
+#include "solve.h"
+#include "f.h"
+#define MAX_IT 10e6
+#define NOT_FOUND (-1)
+#define EPS_VAL 10e-8
+
+int solve(double (*f)(double), double a, double b, double eps, double *x)
+{
+	int it;
+	double c, val_c, val_a = f(a);
+
+	for( it = 0; it<MAX_IT; it++ )
+	{
+		c = (a + b)/2;
+
+		if( (b - a)<eps )
+		{
+			*x = c;
+			return it;
+		}
+
+		if( (a<=c && a>=c) || (b<=c && b>=c) )
+			break;
+
+		val_c = f(c);
+
+		if( (a<=c && a>=c) || (b<=c && b>=c) )
+			break;
+
+		if( val_a*val_c<=0 )
+		{
+			b = c;
+		} else
+		{
+			a = c;
+			val_a = val_c;
+		}
+	}
+
+	return NOT_FOUND;
+}
